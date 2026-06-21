da57be3006c1c078c77027507e41690ea490a62c
03333649169junaid-source <03333649169junaid@gmail.com>
2026-06-21 08:49:28 -0700

    Add chapter3 and chapter4 files

---
+++ chapter4/app.js
@@ -0,0 +1,16 @@
+let studentName = "Mehmil Junaid";
+let age = 18;              // apni age likh do
+let _totalMarks = 450;     // apne marks likh do
+let $fee = 5000;
+let user1 = "Mehmil Junaid";
+
+// 2. Alert se check karo
+alert("Student Name: " + studentName);
+
+// 3. Page pe print karo
+document.write("<h2>Student Details - Mehmil Junaid</h2>");
+document.write("Name: " + studentName + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("Total Marks: " + _totalMarks + "<br>");
+document.write("Fee: " + $fee + "<br>");
+document.write("User ID: " + user1);
